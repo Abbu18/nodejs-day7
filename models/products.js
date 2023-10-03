8c90266874d5036ed4a3d5d9a0147dbef8a423d7
Shaik Abbas <shaikabbas005@gmail.com>
2023-10-03 18:03:43 +0530

    Today's Nodejs Class

---
+++ models/products.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId; 
+
+const products = new Schema({
+    id: ObjectId,
+    productName : {type:String,required:true},
+    productPrice : {type:Number,required:true},
+    productDescription : {type:String,required:true},
+    productDate : {type:String},
+})
+
+
+module.exports = mongoose.model("products",products,"products")
+
